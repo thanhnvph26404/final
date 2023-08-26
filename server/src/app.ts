4c669aa1e6badb045f168ff0eefe76ea92b68a5f
thanhnvph26404 <thanhnvph26404@gmail.com>
2023-08-26 20:52:02 +0700

    feat: add database

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,10 +2,13 @@ import dotenv from "dotenv";
 import express, { Express } from "express";
 import morgan from "morgan";
 import cors from 'cors';
-
+import connectDB from "./config/database";
 const app: Express = express();
 
 dotenv.config();
+
+// connect db
+connectDB(process.env.MONGO_URI as string)
 
 app.use(express.json());
 app.use(morgan("tiny"));
