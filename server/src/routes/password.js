5ce083051e8c378da5bb09f85ac271fad9ed88e7
tridung03 <dunghieu2003@gmail.com>
2023-08-29 22:47:39 +0700

    feat: change password refs: FS-1

--- server/src/routes/password.js
+++ server/src/routes/password.js
@@ -3,7 +3,9 @@ import express from "express";
 import {
   getSecurityCode,
   resetPassword,
- 
+  getCode,
+  checkCode,
+  changePassword,
 } from "../controllers/password";
 
 
@@ -12,8 +14,8 @@ const router = express.Router();
 router.post("/forgot-password", getSecurityCode);
 router.post("/reset-password", resetPassword);
 
-// router.post("/send-code", loginMiddleware, getCode);
-// router.post("/check-code", checkCode);
-// router.post("/change-pass", changePassword);
+router.post("/send-code", getCode);
+router.post("/check-code", checkCode);
+router.post("/change-pass", changePassword);
 
 export default router;
