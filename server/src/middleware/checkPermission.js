5126cc842e71e70b0c05ed212dffda7e4b63f665
giangnhph23819 <giangnhph23819@fpt.edu.vn>
2023-08-30 22:37:11 +0700

    feat:checkpermission refs:FS-6

--- server/src/middleware/checkPermission.js
+++ server/src/middleware/checkPermission.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken"
+import User from "../models/auth"
 export const authMiddlware = async ( req, res, next ) =>
 {
     try
@@ -6,16 +7,35 @@ export const authMiddlware = async ( req, res, next ) =>
         if ( req?.headers?.authorization?.startsWith( "Bearer " ) )
         {
             const token = req.headers.authorization.split( "Bearer " )[ 1 ];
-            console.log( token );
             if ( token )
             {
-                const decoded = jwt.verify( token, process.env.SECRET_KEY )
-                console.log( decoded );
+                const decoded = jwt.verify( token, process.env.SECRET_KEY );
+                const user = await User.findById( decoded?.id );
+                console.log( user );
+
+                req.user = user;
+                next();
+            } else
+            {
+                throw new Error( "bạn chưa đăng nhập " );
             }
         }
     } catch ( error )
     {
         return res.status( 401 ).json( { message: "Lỗi token " } );
 
+    }
+
+}
+export const isAdmin = async ( req, res, next ) =>
+{
+    const { email } = req.user
+    const users = await User.findOne( { email } );
+    if ( users?.role !== "Admin" )
+    {
+        res.status( 401 ).json( { message: "bạn không có quyền truy cập  " } );
+    } else
+    {
+        next()
     }
 }
