2acd2a0b49f5eaf778272f0a9caa931258dda0a0
tuyennn003 <nangtuyen605@gmail.com>
2023-08-31 20:32:55 +0700

    feat: gets products refs: FS-16

--- server/src/app.js
+++ server/src/app.js
@@ -3,7 +3,7 @@ import express from "express";
 import morgan from "morgan";
 import cors from 'cors';
 import connectDB from "./config/database";
-
+import ProductRouter from "./routes/products";
 import authRouter from "./routes/auth";
 import passwordRouter from "./routes/password";
 import categoryRouter from "./routes/categories";
@@ -24,6 +24,7 @@ app.use('/password', passwordRouter)
 app.use('/category', categoryRouter)
 app.use('/images', uploadRouter)
 
+app.use("/products", ProductRouter);
 
 export const viteNodeApp = app;
 // 
--- server/src/routes/products.js
+++ server/src/routes/products.js
@@ -1,10 +1,8 @@
 import express from "express";
-
-import { create } from "../controller/product";
-
+import { create, getAll, getOne } from "../controllers/products";
 const router = express.Router();
 
+router.get("/", getAll);
+router.get("/:id", getOne);
 router.post("/", create);
-
-
 export default router;
