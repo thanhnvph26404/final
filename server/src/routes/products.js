0447c31dc4f4405bd6e7e840e815e10e29d7d462
daivmph25545 <115127976+daivmph25545@users.noreply.github.com>
2023-09-01 11:55:46 +0700

    feat: remove refs: FS-14

--- server/src/routes/products.js
+++ server/src/routes/products.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { authMiddlware, isAdmin } from "../middleware/checkPermission"
 
-import { create, getAll, getOne, updateProduct } from "../controllers/products";
+import { create, getAll, getOne, remove, updateProduct } from "../controllers/products";
 const router = express.Router();
 
 router.get( "/", getAll );
 router.get( "/:id", authMiddlware, isAdmin, getOne );
 router.post( "/", authMiddlware, isAdmin, create );
 router.put( "/:id", authMiddlware, isAdmin, updateProduct );
+router.delete( "/:id", authMiddlware, isAdmin, remove );
 
 export default router;
