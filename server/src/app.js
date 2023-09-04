19ba66e36ef8770c933d1e203ac28b8f050ae3a0
giangnhph23819 <giangnhph23819@fpt.edu.vn>
2023-09-04 22:36:07 +0700

    feat:creat-update-voucher refs:FS-19

--- server/src/app.js
+++ server/src/app.js
@@ -5,6 +5,8 @@ import cors from 'cors';
 import connectDB from "./config/database";
 import ProductRouter from "./routes/products";
 import authRouter from "./routes/auth";
+import voucherRouter from "./routes/voucher";
+
 import passwordRouter from "./routes/password";
 import categoryRouter from "./routes/categories";
 import uploadRouter from "./routes/upload";
@@ -13,18 +15,20 @@ const app = express();
 dotenv.config();
 
 // connect db
-connectDB(process.env.MONGO_URI)
+connectDB( process.env.MONGO_URI )
 
-app.use(express.json());
-app.use(morgan("tiny"));
-app.use(cors())
+app.use( express.json() );
+app.use( morgan( "tiny" ) );
+app.use( cors() )
 
-app.use('/auth', authRouter)
-app.use('/password', passwordRouter)
-app.use('/category', categoryRouter)
-app.use('/images', uploadRouter)
+app.use( '/auth', authRouter )
+app.use( '/password', passwordRouter )
+app.use( '/category', categoryRouter )
+app.use( '/images', uploadRouter )
 
-app.use("/products", ProductRouter);
+app.use( "/products", ProductRouter );
+app.use( '/voucher', voucherRouter )
+
 
 export const viteNodeApp = app;
 // 
