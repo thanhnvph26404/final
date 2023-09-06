0f8d84d33ce5191143e4324f17c782739d52cbfb
tuyennn003 <nangtuyen605@gmail.com>
2023-09-06 22:18:01 +0700

    feat: checkVoucher refs: FS-21

--- server/src/app.js
+++ server/src/app.js
@@ -6,7 +6,6 @@ import connectDB from "./config/database";
 import ProductRouter from "./routes/products";
 import authRouter from "./routes/auth";
 import voucherRouter from "./routes/voucher";
-
 import passwordRouter from "./routes/password";
 import categoryRouter from "./routes/categories";
 import uploadRouter from "./routes/upload";
@@ -15,19 +14,17 @@ const app = express();
 dotenv.config();
 
 // connect db
-connectDB( process.env.MONGO_URI )
+connectDB(process.env.MONGO_URI)
 
-app.use( express.json() );
-app.use( morgan( "tiny" ) );
-app.use( cors() )
-
-app.use( '/auth', authRouter )
-app.use( '/password', passwordRouter )
-app.use( '/category', categoryRouter )
-app.use( '/images', uploadRouter )
-
-app.use( "/products", ProductRouter );
-app.use( '/voucher', voucherRouter )
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(cors())
+app.use('/auth', authRouter)
+app.use('/password', passwordRouter)
+app.use('/category', categoryRouter)
+app.use('/images', uploadRouter)
+app.use("/voucher", voucherRouter);
+app.use("/products", ProductRouter);
 
 
 export const viteNodeApp = app;
