5c02b091890f382a87fc53f8db1128514aa651a5
nvhoan11092003 <nvhoan11092003@gmail.com>
2023-10-25 21:35:26 +0700

    feat : update valueattribute

--- server/src/routes/valueattibute.js
+++ server/src/routes/valueattibute.js
@@ -1,8 +1,9 @@
 import express from "express"
 import { authMiddlware, isAdmin } from "../middleware/checkPermission"
-import { getAll, create, remove, update } from "../controllers/valueattribute"
+import { getAll, create, remove, update ,getbyidatribute } from "../controllers/valueattribute"
 const router = express.Router()
-router.get( "/", getAll )
+router.get("/", getAll)
+router.get("/byidatribute/:id", getbyidatribute )
 router.post( "/",create )
 router.patch( "/:id", update )
 router.delete( "/:id", authMiddlware, isAdmin,remove )
