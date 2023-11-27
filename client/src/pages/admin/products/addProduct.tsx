dfa56e6e54d0e66eabe4bc66b62a6dee0a5ab32f
giangnhph23819 <giangnhph23819@fpt.edu.vn>
2023-11-27 21:19:43 +0700

    feat:update refs:FS-76

--- client/src/pages/admin/products/addProduct.tsx
+++ client/src/pages/admin/products/addProduct.tsx
@@ -12,17 +12,18 @@ import { useGetcolorListQuery } from "../../../store/valueAttribute/colorsevice"
 import { useAddProductMutation } from "../../../store/products/product.services";
 import { toastError, toastSuccess } from "../../../hook/toastify";
 
-const AddProduct = () => {
+const AddProduct = () =>
+{
     const navigate = useNavigate();
-    const { data: categories } = useGetCategoryListQuery([]);
-    const { data: brands } = useGetBrandListQuery([]);
-    const { data: size } = useGetsizeListQuery([])
-    const { data: color } = useGetcolorListQuery([])
+    const { data: categories } = useGetCategoryListQuery( [] );
+    const { data: brands } = useGetBrandListQuery( [] );
+    const { data: size } = useGetsizeListQuery( [] )
+    const { data: color } = useGetcolorListQuery( [] )
 
 
-    const [addProductMutation] = useAddProductMutation();
+    const [ addProductMutation ] = useAddProductMutation();
 
-    const [loadings, setLoadings] = useState(false);
+    const [ loadings, setLoadings ] = useState( false );
 
     const validateMessages = {
         required: '${label} is required!',
@@ -35,35 +36,40 @@ const AddProduct = () => {
         }
     }
 
-    const onFinishFailed = (errorInfo: any) => {
-        console.log("Failed:", errorInfo);
+    const onFinishFailed = ( errorInfo: any ) =>
+    {
+        console.log( "Failed:", errorInfo );
     };
-    const beforeUpload = () => {
+    const beforeUpload = () =>
+    {
         return false;
     };
 
 
 
-    const onSubmit = async (data: any) => {
-        setLoadings(true);
+    const onSubmit = async ( data: any ) =>
+    {
+        setLoadings( true );
         let newurls = [];
 
         // Check if data.images exists and has a fileList property
-        if (data.imgUrl && data.imgUrl.fileList) {
-            newurls = await Promise.all(data.imgUrl.fileList.map(async (item: any) => {
+        if ( data.imgUrl && data.imgUrl.fileList )
+        {
+            newurls = await Promise.all( data.imgUrl.fileList.map( async ( item: any ) =>
+            {
                 const formData = new FormData();
-                formData.append("image", item.originFileObj);
+                formData.append( "image", item.originFileObj );
                 const API_key = "42d2b4a414af48bbc306d6456dd1f943";
                 const apiResponse = await axios.post(
-                    `https://api.imgbb.com/1/upload?key=${API_key}`,
+                    `https://api.imgbb.com/1/upload?key=${ API_key }`,
                     formData
                 );
                 return { uid: apiResponse.data.data.id, url: apiResponse.data.data.url };
-            }))
-            console.log(newurls);
+            } ) )
+            console.log( newurls );
         }
         // console.log( newurls );
-        console.log(data.imgUrl.fileList);
+        console.log( data.imgUrl.fileList );
 
 
 
@@ -77,63 +83,68 @@ const AddProduct = () => {
             category: data.category,
             ProductVariants: data.ProductVariants
         };
-        console.log(newProduct);
+        console.log( newProduct );
 
-        try {
-            await addProductMutation(newProduct).unwrap().then(() => {
-                toastSuccess('Thêm sản phẩm thành công!');
-            }).then(() => {
-                navigate('/admin/products');
-            });
-        } catch (error) {
-            toastError('Thêm sản phẩm thất bại!');
-        } finally {
-            setLoadings(false);
+        try
+        {
+            await addProductMutation( newProduct ).unwrap().then( () =>
+            {
+                toastSuccess( 'Thêm sản phẩm thành công!' );
+            } ).then( () =>
+            {
+                navigate( '/admin/products' );
+            } );
+        } catch ( error )
+        {
+            toastError( 'Thêm sản phẩm thất bại!' );
+        } finally
+        {
+            setLoadings( false );
 
         }
     }
 
 
     return (
-        <div className="w-100" style={{ marginTop: 100, backgroundColor: "white" }}>
-            <h3 style={{ marginTop: 20, marginBottom: 50, color: "black" }}>
+        <div className="w-100" style={ { marginTop: 100, backgroundColor: "white" } }>
+            <h3 style={ { marginTop: 20, marginBottom: 50, color: "black" } }>
                 Thêm sản phẩm
             </h3>
             <Form
                 name="Form"
 
-                labelCol={{ span: 6 }}
-                wrapperCol={{ span: 18 }}
+                labelCol={ { span: 6 } }
+                wrapperCol={ { span: 18 } }
                 // style={{ maxWidth: 800 }}
-                initialValues={{ remember: true }}
-                onFinish={onSubmit}
-                onFinishFailed={onFinishFailed}
-                validateMessages={validateMessages}
+                initialValues={ { remember: true } }
+                onFinish={ onSubmit }
+                onFinishFailed={ onFinishFailed }
+                validateMessages={ validateMessages }
 
             >
                 <Form.Item
                     label="Tên sản Phẩm"
-                    name="_id"
+                    name="name"
                     hidden
-                    rules={[
+                    rules={ [
                         { required: true },
                         { whitespace: true },
                         { min: 6, max: 255 },
-                    ]}
+                    ] }
                     hasFeedback
                 >
                     <Input />
                 </Form.Item>
-                <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-                    <Col className="gutter-row" span={14}>
+                <Row gutter={ { xs: 8, sm: 16, md: 24, lg: 32 } }>
+                    <Col className="gutter-row" span={ 14 }>
                         <Form.Item
                             label="Tên sản Phẩm"
                             name="name"
-                            rules={[
+                            rules={ [
                                 { required: true },
                                 { whitespace: true },
                                 { min: 6, max: 255 },
-                            ]}
+                            ] }
                             hasFeedback
                         >
                             <Input />
@@ -141,9 +152,9 @@ const AddProduct = () => {
                         <Form.Item
                             label="giá gốc"
                             name="price"
-                            rules={[
-                                { required: true, min: 1, max: 100000000, pattern: new RegExp(/^[0-9]+$/), message: "Price is not a valid number" },
-                            ]}
+                            rules={ [
+                                { required: true, min: 1, max: 100000000, pattern: new RegExp( /^[0-9]+$/ ), message: "Price is not a valid number" },
+                            ] }
                             hasFeedback
                         >
                             <Input />
@@ -151,71 +162,71 @@ const AddProduct = () => {
                         <Form.Item
                             label="giá giảm"
                             name="original_price"
-                            rules={[
-                                { min: 1, max: 100000000, pattern: new RegExp(/^[0-9]+$/), message: "originPrice is not a valid number" },
-                            ]}
+                            rules={ [
+                                { min: 1, max: 100000000, pattern: new RegExp( /^[0-9]+$/ ), message: "originPrice is not a valid number" },
+                            ] }
                             hasFeedback
                         >
                             <Input />
                         </Form.Item>
                     </Col>
-                    <Col className="gutter-row" span={8}>
+                    <Col className="gutter-row" span={ 8 }>
                         <Form.Item
                             label="Danh mục"
                             name="category"
-                            labelCol={{ span: 8 }}
-                            rules={[
+                            labelCol={ { span: 8 } }
+                            rules={ [
                                 { required: true },
-                            ]}
+                            ] }
                             hasFeedback
                         >
                             <Select id="" className="mx-2">
-                                {categories?.data?.map((Category: any) => (
-                                    <Select.Option key={Category?._id} value={Category?._id}>
-                                        {Category?.title}
+                                { categories?.data?.map( ( Category: any ) => (
+                                    <Select.Option key={ Category?._id } value={ Category?._id }>
+                                        { Category?.title }
                                     </Select.Option>
-                                ))}
+                                ) ) }
                             </Select>
                         </Form.Item>
                         <Form.Item
                             label="Thương hiệu"
                             name="brand"
-                            labelCol={{ span: 8 }}
-                            rules={[
+                            labelCol={ { span: 8 } }
+                            rules={ [
                                 { required: true },
-                            ]}
+                            ] }
                             hasFeedback
                         >
                             <Select id="" className="mx-2 ">
-                                {brands?.brand?.map((brand: any) => (
-                                    <Select.Option key={brand._id} value={brand._id}>
-                                        {brand.title}
+                                { brands?.brand?.map( ( brand: any ) => (
+                                    <Select.Option key={ brand._id } value={ brand._id }>
+                                        { brand.title }
                                     </Select.Option>
-                                ))}
+                                ) ) }
                             </Select>
                         </Form.Item>
                     </Col>
-                    <Col span={24}>
+                    <Col span={ 24 }>
 
                         <Form.Item
-                            labelCol={{ span: 3 }}
+                            labelCol={ { span: 3 } }
                             label="Chi tiết"
                             name="description"
-                            rules={[
+                            rules={ [
                                 { required: true },
-                            ]}
+                            ] }
                             hasFeedback
                         >
-                            <TextArea cols={40} rows={6} />
+                            <TextArea cols={ 40 } rows={ 6 } />
                         </Form.Item>
                         <Form.Item
                             label="Ảnh sản phẩm"
                             name="imgUrl"
-                            wrapperCol={{ offset: 3, span: 16 }}
-                            rules={[{ required: true, message: "Vui lòng chọn ảnh sản phẩm" }]}
+                            wrapperCol={ { offset: 3, span: 16 } }
+                            rules={ [ { required: true, message: "Vui lòng chọn ảnh sản phẩm" } ] }
                         >
-                            <Upload accept="image/*" listType="picture-card" multiple beforeUpload={beforeUpload} >
-                                <Button icon={<UploadOutlined />} block>
+                            <Upload accept="image/*" listType="picture-card" multiple beforeUpload={ beforeUpload } >
+                                <Button icon={ <UploadOutlined /> } block>
                                     Chọn ảnh
                                 </Button>
                             </Upload>
@@ -228,76 +239,76 @@ const AddProduct = () => {
                 <h1 className="m-2" >Biến thể sản phẩm</h1>
                 <hr className="py-3  " />
                 <Form.List name="ProductVariants">
-                    {(fields, { add, remove }) => (
+                    { ( fields, { add, remove } ) => (
                         <div className="flex flex-col gap-3 gap-x-10    ">
-                            {fields?.map(({ key, name, fieldKey, ...restField }) => (
-                                <Space key={key} className="flex justify-start items-center"
+                            { fields?.map( ( { key, name, fieldKey, ...restField } ) => (
+                                <Space key={ key } className="flex justify-start items-center"
                                 >
                                     <Form.Item
-                                        {...restField}
-                                        labelCol={{ span: 8 }}
-                                        wrapperCol={{ span: 18 }}
+                                        { ...restField }
+                                        labelCol={ { span: 8 } }
+                                        wrapperCol={ { span: 18 } }
                                         className="my-auto "
 
-                                        name={[name, 'color']}  // Đặt tên cho trường "size"
+                                        name={ [ name, 'color' ] }  // Đặt tên cho trường "size"
                                         label="Color"
-                                        rules={[{ required: true, message: 'Color is required' }]}
+                                        rules={ [ { required: true, message: 'Color is required' } ] }
                                     >
                                         <Select id="" className="w-8 mx-3" >
-                                            {color?.data?.map((color: any, index) => (
-                                                <Select.Option key={index} value={color.color}>
-                                                    {color.color}
+                                            { color?.data?.map( ( color: any, index: any ) => (
+                                                <Select.Option key={ index } value={ color.color }>
+                                                    { color.color }
                                                 </Select.Option>
-                                            ))}
+                                            ) ) }
                                         </Select>
                                     </Form.Item>
                                     <Form.Item
                                         className="my-auto ml-3"
 
-                                        labelCol={{ span: 8 }}
-                                        wrapperCol={{ span: 18 }}
-                                        {...restField}
-                                        name={[name, 'size']}  // Đặt tên cho trường "color"
+                                        labelCol={ { span: 8 } }
+                                        wrapperCol={ { span: 18 } }
+                                        { ...restField }
+                                        name={ [ name, 'size' ] }  // Đặt tên cho trường "color"
                                         label="Size"
-                                        rules={[{ required: true, message: 'Size is required' }]}
+                                        rules={ [ { required: true, message: 'Size is required' } ] }
                                     >
                                         <Select id="" className=" mx-3">
-                                            {size?.data?.map((size: any) => (
-                                                <Select.Option value={size.size}>
-                                                    {size.size}
+                                            { size?.data?.map( ( size: any ) => (
+                                                <Select.Option value={ size.size }>
+                                                    { size.size }
                                                 </Select.Option>
-                                            ))}
+                                            ) ) }
                                         </Select>
                                     </Form.Item>
                                     <Form.Item
                                         className="my-auto"
-                                        labelCol={{ span: 8 }}
-                                        wrapperCol={{ span: 18 }}
-                                        {...restField}
-                                        name={[name, 'quantity']}  // Đặt tên cho trường "quantity"
+                                        labelCol={ { span: 8 } }
+                                        wrapperCol={ { span: 18 } }
+                                        { ...restField }
+                                        name={ [ name, 'quantity' ] }  // Đặt tên cho trường "quantity"
                                         label="Số lượng"
-                                        rules={[{ required: true, message: 'Quantity is required' }]}
+                                        rules={ [ { required: true, message: 'Quantity is required' } ] }
                                     >
                                         <Input />
                                     </Form.Item>
-                                    <MinusCircleOutlined onClick={() => { remove(name); }} />
+                                    <MinusCircleOutlined onClick={ () => { remove( name ); } } />
 
                                 </Space>
-                            ))}
+                            ) ) }
                             <Form.Item>
                                 <Button
                                     type="dashed"
-                                    onClick={() => { add(); }}
-                                    icon={<PlusOutlined />}
+                                    onClick={ () => { add(); } }
+                                    icon={ <PlusOutlined /> }
                                 >
                                     Thêm biến thể sản phẩm
                                 </Button>
                             </Form.Item>
                         </div>
-                    )}
+                    ) }
                 </Form.List>
-                <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-                    <Button type="primary" loading={loadings} className="bg-blue-500" htmlType="submit">
+                <Form.Item wrapperCol={ { offset: 8, span: 16 } }>
+                    <Button type="primary" loading={ loadings } className="bg-blue-500" htmlType="submit">
                         cập nhập sản phẩm
                     </Button>
                 </Form.Item>
