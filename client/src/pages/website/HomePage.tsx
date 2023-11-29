56536637f4a7f404aa8cc174fdd2a1e8fd711059
mesm20 <vuminhdai2k3@gmail.com>
2023-11-30 02:57:36 +0700

    feat: fix category Homepage UI refs: FS-84

--- client/src/pages/website/HomePage.tsx
+++ client/src/pages/website/HomePage.tsx
@@ -8,20 +8,18 @@ import { Link } from "react-router-dom";
 import { useGetproductDiscountApiQuery, useGetproductDiscountApiSoldQuery } from "../../store/productDiscount/productDiscont";
 import { useGetCategoryListQuery } from "../../store/categoies/category.services";
 
-const HomePage = () =>
-{
+const HomePage = () => {
   const navigate = useNavigate();
-  const handleGetProductDetail = ( idProduct: string ) =>
-  {
-    navigate( `/home/product-detail/${ idProduct }` )
+  const handleGetProductDetail = (idProduct: string) => {
+    navigate(`/home/product-detail/${idProduct}`)
   }
-  const { data: productList } = useGetProductsQuery( null );
-  const { data: productsold } = useGetproductDiscountApiSoldQuery( null )
-  const { data: productDiscont } = useGetproductDiscountApiQuery( null )
-  const { data: categoies } = useGetCategoryListQuery( null )
-  console.log( productDiscont );
+  const { data: productList } = useGetProductsQuery(null);
+  const { data: productsold } = useGetproductDiscountApiSoldQuery(null)
+  const { data: productDiscont } = useGetproductDiscountApiQuery(null)
+  const { data: categoies } = useGetCategoryListQuery(null)
+  console.log(productDiscont);
 
-  console.log( categoies );
+  console.log(categoies);
   // if (isLoading) {
   //   return <>Loading...</>
   // }
@@ -29,37 +27,32 @@ const HomePage = () =>
 
   return (
     <div className="mx-auto items-center">
-      {/* banner */ }
+      {/* banner */}
       <div className="flex items-center justify-center sm:justify-start">
         <img src="/bannerfirst.jpg" alt="" className="w-full sm:w-auto" />
       </div>
-      {/* Danh mục sản phẩm */ }
+      {/* Danh mục sản phẩm */}
       <h1 className="text-4xl sm:text-6xl font-[Noto sans] text-[#23314B] font-medium md:pt-10 lg:pt-16 text-center">Danh Mục Sản Phẩm</h1>
-      {/* Card */ }
-      <div className="flex space-x-[20px]">
-        { categoies?.data.map( ( category: any ) =>
-        {
-          return (
-
-            <div className="flex flex-col sm:flex-row mx-auto justify-center mt-[80px] mb-10 space-y-6 sm:space-y-0 sm:space-x-6 max-w-screen-xl">
-              <div className="relative w-full sm:w-96 flex-col rounded-xl bg-clip-border text-gray-700 ">
-                <div className="relative h-96 sm:h-100 overflow-hidden rounded-xl bg-clip-border text-gray-700 ">
-                  <div className="object-cover w-full transform scale-100 group-hover:scale-110 transition-transform bg-gray-600">
-                    <img className="object-cover w-full opacity-70" src={ category?.image.url } alt="profile-picture" />
-                    <Link to={ `/products/${ category._id }` }>
-                      <p className="absolute text-white top-[40%] left-[35%] text-[25px] font-medium text-center">{ category?.title }</p>
-                      <p className="text-white pt-20 font-[Noto Sans] text-[20px] font-bold absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity">
-                        <i className="fas fa-chevron-circle-right fa-2x"></i>
-                      </p>
-                    </Link>
-                  </div>
-                </div>
+      <div className="flex space-x-[20px] mt-[20px] ml-[100px]">
+        {categoies?.data.map((category: any) => {
+          return (   
+            <div className="relative w-full sm:w-80 flex-col rounded-xl bg-clip-border text-gray-700 ">
+            <div className="relative h-96 sm:h-100 overflow-hidden rounded-xl bg-clip-border text-gray-700 ">
+              <div className="object-cover w-full transform scale-100 group-hover:scale-110 transition-transform bg-gray-600">
+                <img className="object-cover w-full opacity-70" src={category?.image.url} alt="profile-picture" />
+                <Link to={`/products/${category._id}`}>
+                  <p className="absolute text-white top-[40%]  left-[35%] text-[25px] font-medium text-center">{category?.title}</p>
+                  <p className="text-white pt-20 font-[Noto Sans] text-[20px] font-bold absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity">
+                    <i className="fas fa-chevron-circle-right fa-2x"></i>
+                  </p>
+                </Link>
               </div>
             </div>
-
+          </div>
           )
-        } ) }
+        })}
       </div>
+
 
 
 
@@ -85,82 +78,51 @@ const HomePage = () =>
       </div>
 
 
-      {/* sản phẩm mới */ }
+      {/* sản phẩm mới */}
 
-      <div className="flex flex-wrap">
-        { productList?.products?.map( ( product: any ) =>
-        {
+      <div className="flex flex-wrap space-x-5 ml-[120px]">
+        {productList?.products?.map((product: any) => {
           return (
-            <div className="" key={ product._id }>
+            <div className="" key={product._id}>
               <div className="relative w-full sm:w-96  rounded-xl bg-white bg-clip-border text-gray-700  group">
                 <div className="relative h-100 rounded-xl bg-white bg-clip-border text-gray-700  overflow-hidden group-hover:scale-105 transition-transform duration-300 ease-in-out">
 
-                  { product.original_price && (
+                  {product.original_price && (
                     <p className="absolute z-10 top-3 left-3 bg-[#f83a3a] text-[8px] sm:text-xs font-semibold rounded-full text-white px-2 py-[3px]">
-                      Tiết kiệm { product.price - product.original_price } đ
+                      Tiết kiệm {(product.price - product.original_price).toLocaleString()} đ
                     </p>
-                  ) }
-                  <img className="object-cover w-full" src={ product?.images[ 0 ]?.url } alt="profile-picture" />
-                  <p className="text-center text-[20px] font-semibold mt-2">{ product.name }</p>
+                  )}
+                  <img className="object-cover w-full" src={product?.images[0]?.url} alt="profile-picture" />
+                  <p className="text-center text-[20px] font-semibold mt-2">{product.name}</p>
                   <div className="flex space-x-4 ml-[120px]  mb-4 text-center">
                     <p className="flex space-x-4 mt-2">
-                      {/* Conditional rendering based on the existence of original_price */ }
-                      { product.original_price ? (
+                      {/* Conditional rendering based on the existence of original_price */}
+                      {product.original_price ? (
                         <>
-                          <span className="text-[#f83a3a] text-sm md:text-base font-extralight text-center">{ product.original_price }₫</span>
-                          <span className="line-through text-sm md:text-base font-extralight text-[#23314bb3]">{ product.price }₫</span>
+                          <span className="text-[#f83a3a] text-sm md:text-base font-extralight text-center">{product.original_price.toLocaleString()}₫</span>
+                          <span className="line-through text-sm md:text-base font-extralight text-[#23314bb3]">{product.price.toLocaleString()}₫</span>
                         </>
                       ) : (
-                        <span className=" text-sm md:text-base font-extralight text-align-center text-[#23314bb3]">{ product.price }₫</span>
-                      ) }
+                        <span className=" text-sm md:text-base font-extralight text-align-center text-[#23314bb3]">{product.price.toLocaleString()}₫</span>
+                      )}
                     </p>
                   </div>
-                  <button onClick={ () => handleGetProductDetail( product._id ) } className="absolute top-[70%] right-0 bg-[#23314b] text-white text-center py-2 hover:bg-transparent hover:text-[#23314b] hover:border-2 hover:border-[#23314b] transition-all duration-300 ease-in-out opacity-0 group-hover:opacity-100 rounded-full w-[150px]">
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                  <button onClick={() => handleGetProductDetail(product._id)} className="absolute top-[70%] right-0 bg-[#23314b] text-white text-center py-2 hover:bg-transparent hover:text-[#23314b] hover:border-2 hover:border-[#23314b] transition-all duration-300 ease-in-out opacity-0 group-hover:opacity-100 rounded-full w-[150px]">
                     + Thêm nhanh
                   </button>
                 </div>
               </div>
-
-
-              {/* Card 2 */ }
             </div>
           );
-        } ) }
-
-
-
-
+        })}
       </div>
 
+      
 
 
 
-      {/*sản phẩm bán chạy*/ }
+
+      {/*sản phẩm bán chạy*/}
 
       <div className="grid grid-cols-3 ">
         <div>
@@ -182,35 +144,34 @@ const HomePage = () =>
       </div>
 
 
-      {/* sản phẩm */ }
+      {/* sản phẩm bán chạy */}
 
       <div className="flex flex-wrap">
-        { productsold?.productsSoldOverTwenty?.map( ( product: any ) =>
-        {
+        {productsold?.productsSoldOverTwenty?.map((product: any) => {
           return (
-            <div key={ product._id } className="flex flex-row sm:flex-row mx-auto justify-center mt-10 mb-10 space-y-6 sm:space-y-0 sm:space-x-6 max-w-screen-xl">
-              {/* Card 1 */ }
+            <div key={product._id} className="flex flex-row sm:flex-row mx-auto justify-center mt-10 mb-10 space-y-6 sm:space-y-0 sm:space-x-6 max-w-screen-xl">
+              {/* Card 1 */}
               <div className="relative w-full sm:w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 group">
                 <div className="relative h-100 rounded-xl bg-white bg-clip-border text-gray-700 overflow-hidden group-hover:scale-105 transition-transform duration-300 ease-in-out">
-                  { product.original_price && (
+                  {product.original_price && (
                     <p className="absolute z-10 top-3 left-3 bg-[#f83a3a] text-[8px] sm:text-xs font-semibold rounded-full text-white px-2 py-[3px]">
-                      Tiết kiệm { product.price - product.original_price } đ
+                      Tiết kiệm {product.price - product.original_price} đ
                     </p>
-                  ) }
+                  )}
 
                   <img className="object-cover w-full" src="/sp1.jpg" alt="profile-picture" />
-                  <p className="text-center text-[20px] font-semibold">{ product.name }</p>
+                  <p className="text-center text-[20px] font-semibold">{product.name}</p>
                   <div className="flex space-x-4 pl-[130px] mb-4">
-                    { product.original_price ? (
+                    {product.original_price ? (
                       <>
-                        <span className="text-[#f83a3a] text-sm md:text-base font-extralight text-center">{ product.original_price.toLocaleString() }₫</span>
-                        <span className="line-through text-sm md:text-base font-extralight text-[#23314bb3]">{ product.price.toLocaleString() }₫</span>
+                        <span className="text-[#f83a3a] text-sm md:text-base font-extralight text-center">{product.original_price.toLocaleString()}₫</span>
+                        <span className="line-through text-sm md:text-base font-extralight text-[#23314bb3]">{product.price.toLocaleString()}₫</span>
                       </>
                     ) : (
-                      <span className="text-sm md:text-base font-extralight text-align-center text-[#23314bb3]">{ product.price }₫</span>
-                    ) }
+                      <span className="text-sm md:text-base font-extralight text-align-center text-[#23314bb3]">{product.price.toLocaleString()}₫</span>
+                    )}
                   </div>
-                  <button onClick={ () => handleGetProductDetail( product._id ) } className="absolute top-[70%] right-0 bg-[#23314b] text-white text-center py-2 hover:bg-transparent hover:text-[#23314b] hover:border-2 hover:border-[#23314b] transition-all duration-300 ease-in-out opacity-0 group-hover:opacity-100 rounded-full w-[150px]">
+                  <button onClick={() => handleGetProductDetail(product._id)} className="absolute top-[70%] right-0 bg-[#23314b] text-white text-center py-2 hover:bg-transparent hover:text-[#23314b] hover:border-2 hover:border-[#23314b] transition-all duration-300 ease-in-out opacity-0 group-hover:opacity-100 rounded-full w-[150px]">
                     + Thêm nhanh
                   </button>
                 </div>
@@ -223,7 +184,7 @@ const HomePage = () =>
 
             </div>
           );
-        } ) }
+        })}
       </div>
 
 
@@ -246,49 +207,45 @@ const HomePage = () =>
         </div>
       </div>
 
-      {/* sản phẩm */ }
+      {/* sản phẩm giảm giá*/}
 
       <div className="flex flex-wrap">
-        { productDiscont?.productsWithDiscount?.map( ( product: any ) =>
-        {
+        {productDiscont?.productsWithDiscount?.map((product: any) => {
           return (
-            <div key={ product._id } className="flex flex-row sm:flex-row mx-auto justify-center mt-10 mb-10 space-y-6 sm:space-y-0 sm:space-x-6 max-w-screen-xl">
-              {/* Card 1 */ }
+            <div key={product._id} className="flex flex-row sm:flex-row mx-auto justify-center mt-10 mb-10 space-y-6 sm:space-y-0 sm:space-x-6 max-w-screen-xl">
+              {/* Card 1 */}
               <div className="relative w-full sm:w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 group">
                 <div className="relative h-100 rounded-xl bg-white bg-clip-border text-gray-700 overflow-hidden group-hover:scale-105 transition-transform duration-300 ease-in-out">
 
                   <p className="absolute z-10 top-3 left-3 bg-[#f83a3a] text-[8px] sm:text-xs font-semibold rounded-full text-white px-2 py-[3px]">
-                    Tiết kiệm { product.discountProduct }đ
+                    Tiết kiệm {product.discountProduct.toLocaleString()}đ
                   </p>
 
 
                   <img className="object-cover w-full" src="/sp1.jpg" alt="profile-picture" />
-                  <p className="text-center text-[20px] font-semibold">{ product.name }</p>
+                  <p className="text-center text-[20px] font-semibold">{product.name}</p>
                   <div className="flex space-x-4 pl-[130px] mb-4">
                     <>
-                      <span className="text-[#f83a3a] text-sm md:text-base font-extralight text-center">{ product.original_price.toLocaleString() }₫</span>
-                      <span className="line-through text-sm md:text-base font-extralight text-[#23314bb3]">{ product.price.toLocaleString() }₫</span>
+                      <span className="text-[#f83a3a] text-sm md:text-base font-extralight text-center">{product.original_price.toLocaleString()}₫</span>
+                      <span className="line-through text-sm md:text-base font-extralight text-[#23314bb3]">{product.price.toLocaleString()}₫</span>
                     </>
 
 
                   </div>
-                  <button onClick={ () => handleGetProductDetail( product._id ) } className="absolute top-[70%] right-0 bg-[#23314b] text-white text-center py-2 hover:bg-transparent hover:text-[#23314b] hover:border-2 hover:border-[#23314b] transition-all duration-300 ease-in-out opacity-0 group-hover:opacity-100 rounded-full w-[150px]">
+                  <button onClick={() => handleGetProductDetail(product._id)} className="absolute top-[70%] right-0 bg-[#23314b] text-white text-center py-2 hover:bg-transparent hover:text-[#23314b] hover:border-2 hover:border-[#23314b] transition-all duration-300 ease-in-out opacity-0 group-hover:opacity-100 rounded-full w-[150px]">
                     + Thêm nhanh
                   </button>
                 </div>
-
               </div>
-
-              {/* Card 2 */ }
             </div>
           );
-        } ) }
+        })}
       </div>
 
 
 
 
-      {/* banner dưới */ }
+      {/* banner dưới */}
       <div className="w-full relative text-center mb-[90px] ">
         <img src="/bannerdown.jpg" alt="" className="w-full  sm:w-[1700px] h-[450px] mt-[100px]" />
         <div className="absolute top-[120px] left-[50%] text-white transform -translate-x-1/2">
@@ -303,7 +260,7 @@ const HomePage = () =>
 
       </div>
 
-      {/* một số hình ảnh */ }
+      {/* một số hình ảnh */}
 
       <div className="flex ml-[120px]  space-x-10  mt-8 sm:mt-12">
 
